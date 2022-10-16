8dc173658d013d5d8e4c2425abd7f7bf5cdc72db
Oai-Vu <95222011+Oai-Vu@users.noreply.github.com>
2022-10-16 22:44:31 +0200

    Update to Version 1 of Sunflower.java

--- TP1_Assignment1/src/tp1/p1/logic/gameobjects/Sunflower.java
+++ TP1_Assignment1/src/tp1/p1/logic/gameobjects/Sunflower.java
@@ -1,11 +1,35 @@
 package tp1.p1.logic.gameobjects;
 import tp1.p1.logic.Game;
+import tp1.p1.view.Messages;
 
+/**
+ * Model a sunflower in a game
+ *
+ */
 public class Sunflower {
+	/**
+	 * Initial endurance of a sunflower
+	 */
 	public static final int ENDURANCE = 1;
+	
+	/**
+	 * Number of damage points that a sunflower causes on a zombie
+	 */
 	public static final int DAMAGE = 0;
+	
+	/**
+	 * Number of coins to buy a sunflower
+	 */
 	public static final int COST = 20;
+	
+	/**
+	 * Number of cycles needed for a sunflower to produce new sun coins
+	 */
 	public static final int GROWING_TIME = 3;
+	
+	/**
+	 * Number of sun coins that a sunflower can produce each time
+	 */
 	public static final int PRODUCED_SUN_COINS = 10;
 	
 	private int row;
@@ -15,6 +39,13 @@ public class Sunflower {
 	private boolean isAttackedByZombie;
 	private int currGrowingTime;
 	
+	/**
+	 * Construct and initialize a sunflower 
+	 * 
+	 * @param r row position of the sunflower 
+	 * @param c column position of the sunflower 
+	 * @param currGame the game which the sunflower lives in
+	 */
 	public Sunflower(int r, int c, Game currGame) {
 		row = r;
 		col = c;
@@ -25,28 +56,49 @@ public class Sunflower {
 	}
 	
 	
+	/**
+	 * get the decription of the sunflower
+	 * 
+	 * @return description of the sunflower
+	 */
 	public static String getDescription() {
-		return "";
-	}
+		return Messages.SUNFLOWER_DESCRIPTION.formatted(Sunflower.COST, Sunflower.DAMAGE , Sunflower.ENDURANCE);
+	}	
 	
-	public void getAttackedByZombie(boolean isAttacked) {
-		isAttackedByZombie = isAttacked;
-	}
-	
-	
+	/**
+	 * Check if the sunflower is dead
+	 * 
+	 * @return <code>true</code> if the sunflower is dead
+	 */
 	public boolean isDead() {
 		return remainingLive == 0;
 	}
 	
+	/**
+	 * Get the string representation of the sunflower
+	 */
 	public String toString() {
-		return "S[0" + remainingLive + "]";
+		return Messages.SUNFLOWER_ICON.formatted(remainingLive);
 	}
 	
+	/**
+	 * Check if the sunflower is at the tile <code>(r, c)</code>
+	 * 
+	 * @param r row position of the tile
+	 * @param c column position of the tile 
+	 * 
+	 * @return <code>true</code> if the sunflower is at the tile <code>(r, c)</code>
+	 */
 	public boolean amIAt(int r, int c) {
 		return row == r && col == c;
 	}
 	
 	
+	/**
+	 * Get the number of sun coins produced by the sunflower after the current cycle
+	 * 
+	 * @return the number of sun coins produced by the sunflower after the current cycle
+	 */
 	private int getProducedNewSunCoins() {
 		if (currGrowingTime == Sunflower.GROWING_TIME) {
 			currGrowingTime = 0;
@@ -56,25 +108,30 @@ public class Sunflower {
 	}
 		
 	
+	/**
+	 * Update the state of the sunflower
+	 */
 	public void update() {
 		updateCurrGrowingTime();
 		updateSunCoinsProduction();
 		
 		if (game.checkIfAttackedByZombie(row, col)) {
-			if (col == Game.NUM_COLS - 1) {
-				remainingLive = 0;
-			} else {
-				remainingLive = remainingLive - Zombie.DAMAGE;
-			}
+			remainingLive = remainingLive - Zombie.DAMAGE;
 		}
 	}
 	
+	/**
+	 * Update the current growing time
+	 */
 	private void updateCurrGrowingTime() {
 		if (remainingLive > 0) {
 			currGrowingTime++;
 		}
 	}
 
+	/**
+	 * Update the information about the production of new sun coins and inform it to game
+	 */
 	private void updateSunCoinsProduction() {
 		int receivedSunCoins = getProducedNewSunCoins();
 		if (receivedSunCoins != 0) {
